f03448c42b091d7e0755436f474cb209dd1b5117
abhishek devop <standup787059@gmail.com>
2025-10-12 00:34:35 +0530

    create authMe api

--- auth/src/routes/auth.routes.js
+++ auth/src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const authController = require('../controllers/auth.controller')
 const validator = require('../middlewares/validator.middleware')
+const authMiddleware = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
 router.post('/register', validator.registerUservalidations, authController.registerUser)
 router.post('/login', validator.loginUserValidation, authController.loginUser)
+
+router.get('/me',authMiddleware.authMiddleware, authController.getCurrentUser)
 
 
 module.exports = router
