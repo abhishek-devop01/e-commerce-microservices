86649f308313250b469857c51b68db427eb30b7a
abhishek devop <standup787059@gmail.com>
2025-10-16 23:22:23 +0530

    getProductController

--- product/src/controllers/product.controller.js
+++ product/src/controllers/product.controller.js
@@ -35,6 +35,33 @@ async function createProduct( req,res){
     }
 }
 
+async function getProducts(req, res) {
+
+    const { q, minprice, maxprice, skip = 0, limit = 20 } = req.query;
+
+
+    const filter = {}
+
+    if (q) {
+        filter.$text = { $search: q }
+    }
+
+    if (minprice) {
+        filter[ 'price.amount' ] = { ...filter[ 'price.amount' ], $gte: Number(minprice) }
+    }
+
+    if (maxprice) {
+        filter[ 'price.amount' ] = { ...filter[ 'price.amount' ], $lte: Number(maxprice) }
+    }
+
+    const products = await productModel.find(filter).skip(Number(skip)).limit(Math.min(Number(limit), 20));
+
+    return res.status(200).json({ data: products });
+
+
+}
+
+
 module.exports = {
     createProduct
 }
