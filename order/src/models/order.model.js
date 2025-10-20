9ee5dca1113b491280b4c8d85992b8aa4bfa2dd3
abhishek devop <standup787059@gmail.com>
2025-10-20 20:53:11 +0530

    created orderModel

---
+++ order/src/models/order.model.js
@@ -0,0 +1,71 @@
+// models code
+import { type } from '../../node_modules/raw-body/index.d';
+import { to } from '../../node_modules/sisteransi/src/sisteransi.d';
+
+const mongoose = require('mongoose');
+
+
+
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String,
+    state: String,
+    pincode: String,
+    country: String,
+
+    isDefault: { type: Boolean, default: false }
+});
+
+const orderSchema = new mongoose.Schema({
+
+     user:{
+          type : mongoose.Schema.Types.ObjectId,
+          required : true,
+     },
+     items:[
+          {
+               product:{
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+               },
+               quantity:{
+                    type: Number,
+                    default: 1,
+                    min:1
+               },
+               price:{
+                    amount:{
+                         type: Number,
+                         required: true,
+                    },
+                    currency:{
+                         type: String,
+                         required: true,
+                         enum: ["USD","INR"]
+                    }
+
+               }
+          }
+     ],
+     status:{
+          type: String,
+          enum: ["PENDING","SHIPPED","DELIVERED","CANCELLED","CONFIRMED"],
+     },
+     totalPrice:{
+          amount:{
+               type: Number,
+               required: true,
+          },
+          currency:{
+               type: String,
+               required: true,
+               enum: ["USD","INR"]
+          }
+     },
+     shippingAddress: addressSchema,
+},{ timestamps: true });
+
+
+const orderModel = mongoose.model('order', orderSchema);
+
+module.exports = orderModel;
