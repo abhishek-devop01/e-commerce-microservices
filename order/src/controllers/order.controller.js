b131676b05b3663929090c531695e398305124af
abhishek devop <standup787059@gmail.com>
2025-10-22 23:18:07 +0530

    routes

--- order/src/controllers/order.controller.js
+++ order/src/controllers/order.controller.js
@@ -1,1 +1,11 @@
-// controllers code
+const orderModel = require("../modelss/order.model");
+
+async function createOrder(req, res) {
+
+
+}
+
+
+module.exports = {
+    createOrder
+}
--- order/src/routes/order.routes.js
+++ order/src/routes/order.routes.js
@@ -1,1 +1,17 @@
-// routes code
+const express = require("express");
+const createAuthMiddleware = require("../middlewares/auth.middleware");
+const orderController = require("../controllers/order.controller");
+
+
+const router = express.Router();
+
+router.post('/',authMiddleware(["user"]), )
+
+
+
+
+
+
+
+
+module.exports = router;
