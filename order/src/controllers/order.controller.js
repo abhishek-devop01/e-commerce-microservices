7f981fc73d214d55fdef871374ca5d74f432c103
abhishek devop <standup787059@gmail.com>
2025-10-24 22:37:35 +0530

    fetched data from cart

--- order/src/controllers/order.controller.js
+++ order/src/controllers/order.controller.js
@@ -14,10 +14,18 @@ async function createOrder(req, res) {
             }
         })
 
+        const products = await Promise.all(cartResponse.data.cart.items.map(async(item)=>{
+            return await axios.et(`https://localhost:3001/api/products/${item.productId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+        }))
+
         console.log("Cart response:", cartResponse.data)
 
     }catch(err){
-        console.error("Error fetching cart:", err.message);
+        console.error("Error fetching cart:", err);
         res.status(500).json({
             message: "Internal server error"
         })
