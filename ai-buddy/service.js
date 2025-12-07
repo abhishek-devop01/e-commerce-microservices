52bae4ed3c99d81f6e5feabb6eb29806f4acad5d
abhishek devop <standup787059@gmail.com>
2025-12-07 16:12:33 +0530

    DB connected

--- ai-buddy/service.js
+++ ai-buddy/service.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const app = require('./src/app');
 const http = require('http');
-
 const { initSocketServer } = require('./src/sockets/socket.server');
+const connectDB = require('./src/db/db');
+
+
+
+
 const httpServer = http.createServer(app);
 
 initSocketServer(httpServer);
 
+
+connectDB()
 httpServer.listen(3005, () => {
   console.log('Server is listening on port 3005');
 });
--- ai-buddy/src/sockets/socket.server.js
+++ ai-buddy/src/sockets/socket.server.js
@@ -1,7 +1,26 @@
 const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
 
 async function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
+
+  io.use((socket, next)=>{
+     const cookies = socket.handshake.headers.cookie;
+
+     const {token} = cookie ? cookie.parse(cookies) : {};
+     
+     if(!token){
+           return next(new Error("Token error"));
+     }
+
+     try{
+          const decoded = jwt.verify(token, process.env.JWT_SECRET);
+          socket.user = decoded;
+          next();
+     }catch(err){
+          return next(new Error("Invalid Token"));
+     }
+  })
 
   io.on("connection", (socket) => {
     console.log("New client connected");
