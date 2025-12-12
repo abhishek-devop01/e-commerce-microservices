5d845671f6e5f83f33cd2733ff6ca754ba076e35
abhishek devop <standup787059@gmail.com>
2025-12-12 17:30:10 +0530

    subscribeToQueue function done

--- notification/src/broker/broker.js
+++ notification/src/broker/broker.js
@@ -27,6 +27,22 @@ async function publishToQueue(queueName, data = {}) {
 }
 
 
+async function subscribeToQueue(queueName, callback){
+     if(!channel || !connection) await connect()
+
+     await channel.assertQueue(queueName, {
+          durable: true
+     });
+
+     channel.consume(queueName, async(msg)=>{
+          if (msg !== null){
+               const data = JSON.parse(msg.content.toString());
+               await callback(data);
+               channel.ack(msg)
+          }
+     })
+}
+
 
 
 
@@ -34,5 +50,6 @@ module.exports = {
   connect,
   channel,
   connection,
-  publishToQueue
+  publishToQueue,
+  subscribeToQueue
 };
