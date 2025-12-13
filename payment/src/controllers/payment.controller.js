c9f95263f3f679f05f710b93e37533e3223ba64e
abhishek devop <standup787059@gmail.com>
2025-12-13 21:39:48 +0530

    notification completed

--- payment/src/controllers/payment.controller.js
+++ payment/src/controllers/payment.controller.js
@@ -1,88 +1,121 @@
-const paymentModel = require("../models/payment.model");
-const axios = require("axios");
-require('dotenv').config();
+const paymentModel = require('../models/payment.model');
+const axios = require('axios');
+const { publishToQueue } = require("../broker/broker")
+
 const Razorpay = require('razorpay');
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
+
 async function createPayment(req, res) {
-  const token =
-    req.cookies?.token || req.headers["authorization"]?.split(" ")[1];
-  try {
-    const orderId = req.params.orderId;
+    const token = req.cookies?.token || req.headers?.authorization?.split(' ')[ 1 ];
 
-    const orderResponse = await axios.get(
-      `http://localhost:3003/api/orders/${orderId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-      });
+    try {
 
-     const price = orderResponse.data.order.totalPrice;
-     const order = await razorpay.orders.create(price);
+        const orderId = req.params.orderId;
 
-     const payment = await paymentModel.create({
-      order: orderId,
-      razorpayOrderId: order.id,
-      user: req.user.id,
-      price: {
-        amount: order.amount,
-        currency: order.currency,
-      },
-     });
+        const orderResponse = await axios.get("http://localhost:3003/api/orders/" + orderId, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
 
-      return res.status(201).json({ message: "Payment created successfully", payment });
 
-    
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
+        const price = orderResponse.data.order.totalPrice;
+
+        const order = await razorpay.orders.create(price);
+
+        const payment = await paymentModel.create({
+            order: orderId,
+            razorpayOrderId: order.id,
+            user: req.user.id,
+            price: {
+                amount: order.amount,
+                currency: order.currency
+            }
+        })
+
+        await publishToQueue("PAYMENT_NOTIFICATION.PAYMENT_INITIATED", {
+            email: req.user.email,
+            orderId: orderId,
+            amount: price.amount / 100,
+            currency: price.currency,
+            username: req.user.username,
+        })
+        return res.status(201).json({ message: 'Payment initiated', payment });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+
 }
-
 
 async function verifyPayment(req, res) {
-  const { razorpayOrderId, paymentId, signature } = req.body;
-  const secret =  process.env.RAZORPAY_KEY_SECRET;
+    const { razorpayOrderId, paymentId, signature } = req.body;
+    const secret = process.env.RAZORPAY_KEY_SECRET
 
-  try{
-    const {validatePaymentVerification} = require('../../node_modules/razorpay/dist/utils/razorpay-utils.js');
+    try {
 
-    const isValid = validatePaymentVerification({
-      order_id: razorpayOrderId,
-      payment_id: paymentId,
-    }, signature, secret);
+        const { validatePaymentVerification } = require('../../node_modules/razorpay/dist/utils/razorpay-utils.js')
 
-    if(!isValid){
-      return res.status(400).json({ message: "Invalid payment signature" });
+        const isValid = validatePaymentVerification({
+            order_id: razorpayOrderId,
+            payment_id: paymentId
+        }, signature, secret)
+
+        if (!isValid) {
+            return res.status(400).json({ message: 'Invalid signature' });
+        }
+
+        const payment = await paymentModel.findOne({ razorpayOrderId, status: 'PENDING' });
+
+        if (!payment) {
+            return res.status(404).json({ message: 'Payment not found' });
+        }
+
+        payment.paymentId = paymentId;
+        payment.signature = signature;
+        payment.status = 'COMPLETED';
+
+        await payment.save();
+
+
+        await publishToQueue("PAYMENT_NOTIFICATION.PAYMENT_COMPLETED",
+            {
+                email: req.user.email,
+                orderId: payment.order,
+                paymentId: payment.paymentId,
+                amount: payment.price.amount / 100,
+                currency: payment.price.currency,
+                fullName: req.user.fullName
+            }
+        )
+
+
+
+        res.status(200).json({ message: 'Payment verified successfully', payment });
+
+    } catch (err) {
+        console.log(err);
+
+        await publishToQueue("PAYMENT_NOTIFICATION.PAYMENT_FAILED",
+            {
+                email: req.user.email,
+                paymentId: paymentId,
+                orderId: razorpayOrderId,
+                fullName: req.user.fullName
+            }
+        )
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
-
-    const payment = await paymentModel.findOne({ razorpayOrderId, status: 'PENDING' });
-
-    if(!payment){
-      return res.status(404).json({ message: "Payment not found" });
-    }
-
-    payment.paymentId = paymentId;
-    payment.signature = signature;
-    payment.status = 'COMPLETED';
-
-    await payment.save();
-
-    res.status(200).json({ message: "Payment verified successfully", payment });
-
-
-
-  }catch(e){
-    console.log(e);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
 }
+
+
 module.exports = {
     createPayment,
     verifyPayment
-};
+}
